5f0d49e1a2f11cabb7ea5dcca07ac47316ab2d2f
likaon7 <77878968+likaon7@users.noreply.github.com>
2022-05-21 19:49:38 +0200

    Update map.js

--- js/map.js
+++ js/map.js
@@ -1,4 +1,4 @@
-var baseURL = 'https://5.180.181.48/'
+var baseURL = 'https://5.180.181.48/' //fileserver with pregenerated data (change to yours)
 
 Number.prototype.map = function(in_min, in_max, out_min, out_max) {
 	return (this - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
